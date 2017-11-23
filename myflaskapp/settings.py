8287076b8f43c7d66f9800bf063835bc8ac260eb
Austin McConnell <austin.s.mcconnell@gmail.com>
2017-11-22 20:13:02 -0600

    Changed env vars read from in settings file

--- myflaskapp/settings.py
+++ myflaskapp/settings.py
@@ -6,7 +6,7 @@ import os
 class Config(object):
     """Base configuration."""
 
-    SECRET_KEY = os.environ.get('MYFLASKAPP_SECRET')
+    SECRET_KEY = os.environ.get('SECRET_TOKEN')
     APP_DIR = os.path.abspath(os.path.dirname(__file__))  # This directory
     PROJECT_ROOT = os.path.abspath(os.path.join(APP_DIR, os.pardir))
     BCRYPT_LOG_ROUNDS = 13
