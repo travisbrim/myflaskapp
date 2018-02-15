065aa62511782b73526198472a2819ba4a1d47e0
Travis Brim <me@travisbrim.com>
2018-02-14 20:15:03 -0600

    Added /products/ GET endpoint
    Endpoint for returning list of all products with sorting & filtering implemented

--- myflaskapp/product/views.py
+++ myflaskapp/product/views.py
@@ -1,9 +1,26 @@
 # -*- coding: utf-8 -*-
 """User views."""
-from flask import Blueprint
+from flask import Blueprint, jsonify, request
 
 from myflaskapp.product.models import Product
 
 bp = Blueprint(name='product',  # pylint: disable=invalid-name
                import_name=__name__,
                )
+
+@bp.route('/products/', methods=['GET'])
+def get_products():
+
+    ALLOWED_SORT = ['-price', 'price']
+
+    sort = request.args.get('sort')
+    if sort and sort not in ALLOWED_SORT:
+        return jsonify({'error': 'invalid sort'}), 403
+
+    products = Product.query.filter().order_by(sort)
+
+    filter_str = request.args.get('filter')
+    if filter_str:
+        products = products.filter(Product.name.ilike(filter_str) | Product.description.ilike(filter_str)).all()
+
+    return jsonify({'data': [product.serialize() for product in products]}), 200
