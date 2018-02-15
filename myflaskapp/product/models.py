3d1aa5a62f4324823e5e0d506b39aab1e23db481
Travis Brim <me@travisbrim.com>
2018-02-15 07:31:52 -0600

    Added price validation to model, style cleanup

--- myflaskapp/product/models.py
+++ myflaskapp/product/models.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 """Product models."""
+from decimal import Decimal
+
+from sqlalchemy.orm import validates
 
 from myflaskapp.database import Column, Model, SurrogatePK, db
 
@@ -12,13 +15,20 @@ class Product(SurrogatePK, Model):
 
     def __init__(self, name, price, description=None, color=None, **kwargs):
         """Create instance."""
-        db.Model.__init__(self, name=name, description=description, price=price, color=color, **kwargs)
+        db.Model.__init__(self, name=name, description=description,
+            price=price, color=color, **kwargs)
 
     def serialize(self):
         return {
+            "id": self.id,
             "name": self.name,
             "description": self.description,
             # TODO: introduces the potential for rounding errors
             "price": float(self.price),
             "color": self.color
         }
+
+    @validates('price')
+    def validate_price(self, key, price):
+        assert Decimal(price) >= 0
+        return price
