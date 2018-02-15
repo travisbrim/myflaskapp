053089527d1dd1524d0f155079a6282be6398a56
Travis Brim <me@travisbrim.com>
2018-02-14 20:29:14 -0600

    Added /products/id GET endpoint
    Endpoint for returning single product

--- myflaskapp/product/views.py
+++ myflaskapp/product/views.py
@@ -9,7 +9,7 @@ bp = Blueprint(name='product',  # pylint: disable=invalid-name
                )
 
 @bp.route('/products/', methods=['GET'])
-def get_products():
+def retrieve_products():
 
     ALLOWED_SORT = ['-price', 'price']
 
@@ -24,3 +24,11 @@ def get_products():
         products = products.filter(Product.name.ilike(filter_str) | Product.description.ilike(filter_str)).all()
 
     return jsonify({'data': [product.serialize() for product in products]}), 200
+
+@bp.route('/products/<id>', methods=['GET'])
+def retrieve_product(id):
+    product = Product.query.get(id)
+    if product:
+        return jsonify({'data': product.serialize()}), 200
+    else:
+        return jsonify({'error': 'resource does not exist'}), 403
